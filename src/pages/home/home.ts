39812121fbc758745e256d9cbf8afdb224e7c95d
Prasheel <prasheel.soni@hotwaxsystems.com>
2018-02-18 15:14:50 +0530

    Formatting for Pages, questions and Answers

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import {AlertController, NavController} from 'ionic-angular';
-import * as pdfMake from 'pdfmake/build/pdfmake';
-import * as pdfFonts from 'pdfmake/build/vfs_fonts';
+import  pdfMake from 'pdfmake/build/pdfmake';
+import  pdfFonts from 'pdfmake/build/vfs_fonts';
 
 
 @Component({
@@ -12,9 +12,32 @@ export class HomePage {
 
   questionList:any=[];
   question:any={};
+  dd;
 
 
   constructor(public navCtrl: NavController, public alertCtrl:AlertController) {
+    this.dd = {
+      header: {text:'Test Paper', style: 'header'},
+      content: [],
+
+      styles: {
+        header: {
+          fontSize: 22,
+          alignment:'center',
+          bold: true,
+
+        },
+        question: {
+          fontSize: 18,
+          alignment: 'left',
+          margin: [0, 20, 0, 20]
+        },
+        options: {
+          fontSize: 16,
+          alignment: 'left'
+        }
+      }
+    };
   }
 
   addQuestion() {
@@ -55,42 +78,20 @@ export class HomePage {
           handler: data => {
             this.question = {'question':data.question,'option1':data.option1,'option2':data.option2,'option3':data.option3,'option4':data.option4}
             this.questionList.push(this.question);
-            console.log(this.questionList);
-            this.downloadPDF();
+            this.dd.content.push({text: this.question.question, style: 'question'}, {ol: [{text: this.question.option1},
+              {text: this.question.option2},
+              {text: this.question.option3},
+              {text: this.question.option4}], style: 'options'});
           }
         }
       ]
     });
-   prompt.present();
+    prompt.present();
 
-   }
-
-
-  downloadPDF() {
-
-      for(let question of this.questionList) {
-
-        pdfMake.vfs = pdfFonts.pdfMake.vfs;
-        let dd = {
-          content: [
-            {text: 'Test Paper', style: 'header'},
-            {text: question.question}
-          ],
-
-          styles: {
-            header: {
-              fontSize: 22,
-              alignment:'center',
-              bold: true
-            }
-          }
-        };
-        pdfMake.createPdf(dd).download();
-      }
   }
 
-
-
-
-
+  downloadPDF() {
+    pdfMake.vfs = pdfFonts.pdfMake.vfs;
+    pdfMake.createPdf(this.dd).download();
+  }
 }
